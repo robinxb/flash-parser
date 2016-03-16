c6be3453d61257a0a5edf1b8e6ca10aa663ac715
robin <robinxiaobin@gmail.com>
2016-03-16 12:16:39 +0800

    massive improve processing speed (50% up)

--- run.py
+++ run.py
@@ -39,6 +39,44 @@ def show_exception_and_exit(exc_type, exc_value, tb):
     remove_temp_dirs_when_except();
     sys.exit(-1)
 
+import struct
+import imghdr
+
+def get_image_size(fname):
+    '''Determine the image type of fhandle and return its size.
+    from draco'''
+    with open(fname, 'rb') as fhandle:
+        head = fhandle.read(24)
+        if len(head) != 24:
+            return
+        if imghdr.what(fname) == 'png':
+            check = struct.unpack('>i', head[4:8])[0]
+            if check != 0x0d0a1a0a:
+                return
+            width, height = struct.unpack('>ii', head[16:24])
+        elif imghdr.what(fname) == 'gif':
+            width, height = struct.unpack('<HH', head[6:10])
+        elif imghdr.what(fname) == 'jpeg':
+            try:
+                fhandle.seek(0) # Read 0xff next
+                size = 2
+                ftype = 0
+                while not 0xc0 <= ftype <= 0xcf:
+                    fhandle.seek(size, 1)
+                    byte = fhandle.read(1)
+                    while ord(byte) == 0xff:
+                        byte = fhandle.read(1)
+                    ftype = ord(byte)
+                    size = struct.unpack('>H', fhandle.read(2))[0] - 2
+                # We are at a SOFn block
+                fhandle.seek(1, 1)  # Skip `precision' byte.
+                height, width = struct.unpack('>HH', fhandle.read(4))
+            except Exception: #IGNORE:W0703
+                return
+        else:
+            return
+        return width, height
+
 sys.excepthook = show_exception_and_exit
 
 this_file = inspect.getfile(inspect.currentframe())
@@ -173,13 +211,13 @@ class MainTree():
 
     def WaitJSDone(self, filepath, bRemove = False):
         while(not os.path.exists(filepath)):
-            time.sleep(1)
+            time.sleep(0.5)
         if bRemove:
             while os.path.exists(filepath):
                 try:
                     os.remove(filepath)
                 except OSError:
-                    time.sleep(1)
+                    time.sleep(0.5)
                 finally:
                     return
 
@@ -212,7 +250,7 @@ class MainTree():
             try:
                 shutil.rmtree(self.tmpPath)
             except OSError:
-                time.sleep(1)
+                time.sleep(0.5)
             finally:
                 return
 
@@ -257,10 +295,7 @@ class MainTree():
         if sysType == "Windows":
             identify_path = SCRIPT_PATH + SEP + 'identify.exe '
         for k in files:
-            cmd = '%s -format "%%[fx:w]x%%[fx:h]" "%s"'%(identify_path, imgpath + '%s%s'%(os.path.sep ,k))
-            sts, imgSizeStr = self.ExecuteCmd(cmd)
-            assert(imgSizeStr)
-            w, h = imgSizeStr.split('x')
+            w, h = get_image_size(imgpath + '%s%s'%(os.path.sep ,k))
             w, h = int(w), int(h)
             self.originImgSize[k] = '{"w": %s, "h":%s}'%(w, h)
             main_name, ext = os.path.splitext(k)
@@ -412,4 +447,5 @@ if __name__ == '__main__':
 
     root = loadFiles()
     root.Run()
+
 
